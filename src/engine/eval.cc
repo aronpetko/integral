da665873d46f8d8b44e5e3426616e99c8f4838b1
Aron Petkovski <aronpetko@gmail.com>
2024-05-26 19:31:14 -0500

    kms

--- src/engine/eval.cc
+++ src/engine/eval.cc
@@ -89,31 +89,31 @@ bool StaticExchange(Move move, int threshold, const BoardState &state) {
 
     if ((next_attacker = our_attackers & pawns)) {
       attacker_value = kSEEPieceScores[PieceType::kPawn];
-      occupied.ClearBit(next_attacker.GetLsb());
+      occupied.ClearBit(next_attacker.GetLSB());
 
       // Add pieces that were diagonal xray attacking the captured piece
       bishop_attacks = move_gen::BishopMoves(to, occupied);
       all_attackers |= bishop_attacks & (bishops | queens);
     } else if ((next_attacker = our_attackers & knights)) {
       attacker_value = kSEEPieceScores[PieceType::kKnight];
-      occupied ^= BitBoard::FromSquare(next_attacker.GetLsb());
+      occupied ^= BitBoard::FromSquare(next_attacker.GetLSB());
     } else if ((next_attacker = our_attackers & bishops)) {
       attacker_value = kSEEPieceScores[PieceType::kBishop];
-      occupied.ClearBit(next_attacker.GetLsb());
+      occupied.ClearBit(next_attacker.GetLSB());
 
       // Add pieces that were xray attacking the captured piece
       bishop_attacks = move_gen::BishopMoves(to, occupied);
       all_attackers |= bishop_attacks & (bishops | queens);
     } else if ((next_attacker = our_attackers & rooks)) {
       attacker_value = kSEEPieceScores[PieceType::kRook];
-      occupied.ClearBit(next_attacker.GetLsb());
+      occupied.ClearBit(next_attacker.GetLSB());
 
       // Add pieces that were xray attacking the captured piece
       rook_attacks = move_gen::RookMoves(to, occupied);
       all_attackers |= rook_attacks & (rooks | queens);
     } else if ((next_attacker = our_attackers & queens)) {
       attacker_value = kSEEPieceScores[PieceType::kQueen];
-      occupied.ClearBit(next_attacker.GetLsb());
+      occupied.ClearBit(next_attacker.GetLSB());
 
       // Add pieces that were xray attacking the captured piece
       rook_attacks = move_gen::RookMoves(to, occupied);
@@ -142,104 +142,11 @@ bool StaticExchange(Move move, int threshold, const BoardState &state) {
   return state.turn == winner;
 }
 
-// clang-format off
-constexpr std::array<ScorePair, PieceType::kNumTypes> kPieceValues = {
-    PAIR(100, 110),  // Pawn
-    PAIR(300, 280),  // Knight
-    PAIR(305, 295),  // Bishop
-    PAIR(500, 520),  // Rook
-    PAIR(900, 880),  // Queen
-    PAIR(0, 0)       // King
-};
+constexpr std::array kPieceValues = {100, 300, 305, 500, 900};
 
-using PieceSquareTable = std::array<std::array<ScorePair, Square::kSquareCount>,
-                                    PieceType::kNumTypes>;
+int EvaluateMaterial(const BoardState &state) {
+  int material = 0;
 
-constexpr PieceSquareTable kPieceSquareTables = {{
-  { // Pawns
-    PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0),
-    PAIR(50, 50), PAIR(50, 50), PAIR(50, 50), PAIR(50, 50), PAIR(50, 50), PAIR(50, 50), PAIR(50, 50), PAIR(50, 50),
-    PAIR(10, 20), PAIR(10, 20), PAIR(20, 30), PAIR(30, 40), PAIR(30, 40), PAIR(20, 30), PAIR(10, 20), PAIR(10, 20),
-    PAIR(5, 10), PAIR(5, 15), PAIR(10, 25), PAIR(25, 35), PAIR(25, 35), PAIR(10, 25), PAIR(5, 15), PAIR(5, 10),
-    PAIR(0, 5), PAIR(0, 10), PAIR(0, 20), PAIR(20, 30), PAIR(20, 30), PAIR(0, 20), PAIR(0, 10), PAIR(0, 5),
-    PAIR(5, 0), PAIR(-5, 5), PAIR(-10, 10), PAIR(0, 20), PAIR(0, 20), PAIR(-10, 10), PAIR(-5, 5), PAIR(5, 0),
-    PAIR(5, 5), PAIR(10, 10), PAIR(10, 10), PAIR(-20, -5), PAIR(-20, -5), PAIR(10, 10), PAIR(10, 10), PAIR(5, 5),
-    PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0)
-  },
-  { // Knights
-     PAIR(-50, -50), PAIR(-40, -40), PAIR(-30, -30), PAIR(-30, -30), PAIR(-30, -30), PAIR(-30, -30), PAIR(-40, -40), PAIR(-50, -50),
-     PAIR(-40, -40), PAIR(-20, -20), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(-20, -20), PAIR(-40, -40),
-     PAIR(-30, -30), PAIR(0, 5), PAIR(10, 20), PAIR(15, 25), PAIR(15, 25), PAIR(10, 20), PAIR(0, 5), PAIR(-30, -30),
-     PAIR(-30, -30), PAIR(5, 10), PAIR(15, 25), PAIR(20, 30), PAIR(20, 30), PAIR(15, 25), PAIR(5, 10), PAIR(-30, -30),
-     PAIR(-30, -30), PAIR(0, 5), PAIR(15, 25), PAIR(20, 30), PAIR(20, 30), PAIR(15, 25), PAIR(0, 5), PAIR(-30, -30),
-     PAIR(-30, -30), PAIR(5, 10), PAIR(10, 20), PAIR(15, 25), PAIR(15, 25), PAIR(10, 20), PAIR(5, 10), PAIR(-30, -30),
-     PAIR(-40, -40), PAIR(-20, -20), PAIR(0, 5), PAIR(5, 10), PAIR(5, 10), PAIR(0, 5), PAIR(-20, -20), PAIR(-40, -40),
-     PAIR(-50, -50), PAIR(-40, -40), PAIR(-30, -30), PAIR(-30, -30), PAIR(-30, -30), PAIR(-30, -30), PAIR(-40, -40), PAIR(-50, -50)
-  }, // Bishops
-  {
-     PAIR(-20, -20), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-20, -20),
-     PAIR(-10, -10), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(-10, -10),
-     PAIR(-10, -10), PAIR(0, 5), PAIR(5, 10), PAIR(10, 15), PAIR(10, 15), PAIR(5, 10), PAIR(0, 5), PAIR(-10, -10),
-     PAIR(-10, -10), PAIR(5, 10), PAIR(5, 15), PAIR(10, 25), PAIR(10, 25), PAIR(5, 15), PAIR(5, 10), PAIR(-10, -10),
-     PAIR(-10, -10), PAIR(0, 5), PAIR(10, 15), PAIR(10, 25), PAIR(10, 25), PAIR(10, 15), PAIR(0, 5), PAIR(-10, -10),
-     PAIR(-10, -10), PAIR(10, 10), PAIR(10, 15), PAIR(10, 15), PAIR(10, 15), PAIR(10, 15), PAIR(10, 10), PAIR(-10, -10),
-     PAIR(-10, -10), PAIR(5, 0), PAIR(0, 5), PAIR(0, 10), PAIR(0, 10), PAIR(0, 5), PAIR(5, 0), PAIR(-10, -10),
-     PAIR(-20, -20), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-10, -10), PAIR(-20, -20)
-  },
-  { // Rooks
-     PAIR(0, 10), PAIR(0, 15), PAIR(0, 15), PAIR(0, 20), PAIR(0, 20), PAIR(0, 15), PAIR(0, 10), PAIR(0, 5),
-     PAIR(5, 5), PAIR(10, 10), PAIR(10, 10), PAIR(10, 15), PAIR(10, 15), PAIR(10, 10), PAIR(10, 5), PAIR(5, 0),
-     PAIR(-5, 5), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 5), PAIR(-5, 0),
-     PAIR(-5, 0), PAIR(0, 5), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 5), PAIR(0, 0), PAIR(-5, -5),
-     PAIR(-5, -5), PAIR(0, 0), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(0, 0), PAIR(-5, -5), PAIR(-5, -10),
-     PAIR(-5, -10), PAIR(0, -5), PAIR(0, 0), PAIR(0, 0), PAIR(0, 0), PAIR(0, -5), PAIR(-5, -10), PAIR(-5, -15),
-     PAIR(-5, -15), PAIR(0, -10), PAIR(0, -5), PAIR(0, 0), PAIR(0, 0), PAIR(0, -5), PAIR(-5, -10), PAIR(-5, -20),
-     PAIR(0, -20), PAIR(0, -15), PAIR(0, -10), PAIR(5, -5), PAIR(5, -5), PAIR(0, -10), PAIR(0, -15), PAIR(0, -20)
-  },
-  { // Queens
-     PAIR(-20, -20), PAIR(-10, -10), PAIR(-10, -10), PAIR(-5, -5), PAIR(-5, -5), PAIR(-10, -10), PAIR(-10, -10), PAIR(-20, -20),
-     PAIR(-10, -10), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(0, 5), PAIR(-10, -10),
-     PAIR(-10, -5), PAIR(0, 10), PAIR(5, 10), PAIR(5, 10), PAIR(5, 10), PAIR(5, 10), PAIR(0, 10), PAIR(-10, -5),
-     PAIR(-5, 0), PAIR(0, 10), PAIR(5, 15), PAIR(5, 20), PAIR(5, 20), PAIR(5, 15), PAIR(0, 10), PAIR(-5, 0),
-     PAIR(0, 5), PAIR(0, 15), PAIR(5, 20), PAIR(5, 25), PAIR(5, 25), PAIR(5, 20), PAIR(0, 15), PAIR(-5, 5),
-     PAIR(-10, -5), PAIR(5, 10), PAIR(5, 20), PAIR(5, 20), PAIR(5, 20), PAIR(5, 20), PAIR(0, 10), PAIR(-10, -5),
-     PAIR(-10, -10), PAIR(0, 5), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 10), PAIR(0, 5), PAIR(-10, -10),
-     PAIR(-20, -20), PAIR(-10, -10), PAIR(-10, -5), PAIR(-5, 0), PAIR(-5, 0), PAIR(-10, -5), PAIR(-10, -10), PAIR(-20, -20)
-  },
-  { // King
-     PAIR(-30, 30), PAIR(-40, 40), PAIR(-40, 40), PAIR(-50, 50), PAIR(-50, 50), PAIR(-40, 40), PAIR(-40, 40), PAIR(-30, 30),
-     PAIR(-30, 20), PAIR(-40, 20), PAIR(-40, 30), PAIR(-50, 40), PAIR(-50, 40), PAIR(-40, 30), PAIR(-40, 20), PAIR(-30, 20),
-     PAIR(-30, 10), PAIR(-40, 10), PAIR(-40, 20), PAIR(-50, 30), PAIR(-50, 30), PAIR(-40, 20), PAIR(-40, 10), PAIR(-30, 10),
-     PAIR(-30, 0), PAIR(-40, 5), PAIR(-40, 15), PAIR(-50, 30), PAIR(-50, 30), PAIR(-40, 15), PAIR(-40, 5), PAIR(-30, 0),
-     PAIR(-20, -10), PAIR(-30, 0), PAIR(-30, 10), PAIR(-40, 20), PAIR(-40, 20), PAIR(-30, 10), PAIR(-30, 0), PAIR(-20, -10),
-     PAIR(-10, -20), PAIR(-20, -5), PAIR(-20, 5), PAIR(-20, 10), PAIR(-20, 10), PAIR(-20, 5), PAIR(-20, -5), PAIR(-10, -20),
-     PAIR(20, -30), PAIR(20, -10), PAIR(0, -10), PAIR(0, 0), PAIR(0, 0), PAIR(0, -10), PAIR(20, -10), PAIR(20, -30),
-     PAIR(20, -50), PAIR(30, -30), PAIR(10, -30), PAIR(0, -20), PAIR(0, -20), PAIR(10, -30), PAIR(30, -30), PAIR(20, -50)
-  },
-}};
-
-constexpr std::array<ScorePair, 9> kKnightMobility = {
-    PAIR(-5, -20), PAIR(5, 10), PAIR(23, 28), PAIR(29, 34), PAIR(36, 41),
-    PAIR(40, 45), PAIR(48, 53), PAIR(48, 53), PAIR(51, 56)
-};
-
-constexpr std::array<ScorePair, 15> kRookMobility = {
-    PAIR(-10, 0), PAIR(-5, 5), PAIR(0, 10), PAIR(5, 15), PAIR(5, 15),
-    PAIR(10, 20), PAIR(13, 23), PAIR(18, 28), PAIR(25, 35), PAIR(34, 44),
-    PAIR(38, 48), PAIR(42, 52), PAIR(47, 57), PAIR(50, 60), PAIR(55, 65)
-};
-
-constexpr std::array<ScorePair, 14> kBishopMobility = {
-    PAIR(3, 8), PAIR(12, 17), PAIR(21, 26), PAIR(23, 28),
-    PAIR(31, 36), PAIR(39, 44), PAIR(46, 51), PAIR(50, 55),
-    PAIR(50, 55), PAIR(53, 58), PAIR(56, 61), PAIR(60, 65),
-    PAIR(59, 64), PAIR(92, 97)
-};
-
-constexpr std::array<int, PieceType::kNumTypes> kGamePhaseIncrements = {0, 1, 1, 2, 4, 0};
-// clang-format on
-
-std::pair<ScorePair, int> EvaluateMaterialAndPhase(const BoardState &state) {
   const Color us = state.turn, them = FlipColor(us);
 
   const BitBoard our_pieces = state.Occupied(us);
@@ -262,135 +169,18 @@ std::pair<ScorePair, int> EvaluateMaterialAndPhase(const BoardState &state) {
   const int queen_count =
       (queens & our_pieces).PopCount() - (queens & their_pieces).PopCount();
 
-  ScorePair score;
-  score += kPieceValues[PieceType::kPawn] * pawn_count;
-  score += kPieceValues[PieceType::kKnight] * knight_count;
-  score += kPieceValues[PieceType::kBishop] * bishop_count;
-  score += kPieceValues[PieceType::kRook] * rook_count;
-  score += kPieceValues[PieceType::kQueen] * queen_count;
+  material += kPieceValues[PieceType::kPawn] * pawn_count;
+  material += kPieceValues[PieceType::kKnight] * knight_count;
+  material += kPieceValues[PieceType::kBishop] * bishop_count;
+  material += kPieceValues[PieceType::kRook] * rook_count;
+  material += kPieceValues[PieceType::kQueen] * queen_count;
 
-  int phase = 0;
-  phase += kGamePhaseIncrements[PieceType::kKnight] * knights.PopCount();
-  phase += kGamePhaseIncrements[PieceType::kBishop] * bishops.PopCount();
-  phase += kGamePhaseIncrements[PieceType::kRook] * rooks.PopCount();
-  phase += kGamePhaseIncrements[PieceType::kQueen] * queens.PopCount();
-
-  return {score, phase};
-}
-
-ScorePair EvaluatePieceSquares(const BoardState &state) {
-  ScorePair score;
-  const Color us = state.turn, them = FlipColor(us);
-
-  BitBoard our_pieces = state.Occupied(us);
-  while (our_pieces) {
-    const auto square = Square(our_pieces.PopLsb());
-    const auto piece_type = state.GetPieceType(square);
-    score += kPieceSquareTables[piece_type][RelativeSquare(square, us)];
-  }
-
-  BitBoard their_pieces = state.Occupied(them);
-  while (their_pieces) {
-    const auto square = Square(their_pieces.PopLsb());
-    const auto piece_type = state.GetPieceType(square);
-    score -= kPieceSquareTables[piece_type][RelativeSquare(square, them)];
-  }
-
-  return score;
-}
-
-ScorePair EvaluateKnights(const BoardState &state) {
-  ScorePair score;
-  const Color us = state.turn, them = FlipColor(us);
-  const BitBoard our_pieces = state.Occupied(us),
-                 their_pieces = state.Occupied(them);
-
-  BitBoard our_knights = state.Knights(us);
-  while (our_knights) {
-    const auto square = Square(our_knights.PopLsb());
-    const auto moves = move_gen::KnightMoves(square) & ~our_pieces;
-    score += kKnightMobility[moves.PopCount()];
-  }
-
-  BitBoard their_knights = state.Knights(them);
-  while (their_knights) {
-    const auto square = Square(their_knights.PopLsb());
-    const auto moves = move_gen::KnightMoves(square) & ~their_pieces;
-    score -= kKnightMobility[moves.PopCount()];
-  }
-
-  return score;
-}
-
-ScorePair EvaluateRooks(const BoardState &state) {
-  ScorePair score;
-  const Color us = state.turn, them = FlipColor(us);
-  const BitBoard our_pieces = state.Occupied(us),
-                 their_pieces = state.Occupied(them),
-                 occupied = our_pieces | their_pieces;
-
-  BitBoard our_rooks = state.Rooks(us);
-  while (our_rooks) {
-    const auto square = Square(our_rooks.PopLsb());
-    const auto moves = move_gen::RookMoves(square, occupied) & ~our_pieces;
-    score += kRookMobility[moves.PopCount()];
-  }
-
-  BitBoard their_rooks = state.Rooks(them);
-  while (their_rooks) {
-    const auto square = Square(their_rooks.PopLsb());
-    const auto moves = move_gen::RookMoves(square, occupied) & ~their_pieces;
-    score -= kRookMobility[moves.PopCount()];
-  }
-
-  return score;
-}
-
-ScorePair EvaluateBishops(const BoardState &state) {
-  ScorePair score;
-  const Color us = state.turn, them = FlipColor(us);
-  const BitBoard our_pieces = state.Occupied(us),
-                 their_pieces = state.Occupied(them),
-                 occupied = our_pieces | their_pieces;
-
-  BitBoard our_bishops = state.Bishops(us);
-  while (our_bishops) {
-    const auto square = Square(our_bishops.PopLsb());
-    const auto moves = move_gen::BishopMoves(square, occupied) & ~our_pieces;
-    score += kBishopMobility[moves.PopCount()];
-  }
-
-  BitBoard their_bishops = state.Bishops(them);
-  while (their_bishops) {
-    const auto square = Square(their_bishops.PopLsb());
-    const auto moves = move_gen::BishopMoves(square, occupied) & ~their_pieces;
-    score -= kBishopMobility[moves.PopCount()];
-  }
-
-  return score;
-}
-
-double Lerp(double a, double b, double f) {
-  return a * (1.0 - f) + (b * f);
+  return material;
 }
 
 Score Evaluate(const BoardState &state) {
-  const int kMaxPhase = 24;
-
-  // Use int for intermediate calculations to prevent overflow
-  auto [material_score, phase] = EvaluateMaterialAndPhase(state);
-  auto score_pair = material_score + EvaluatePieceSquares(state) +
-                    EvaluateKnights(state) + EvaluateRooks(state) +
-                    EvaluateBishops(state);
-
-  phase = std::min(phase, kMaxPhase);
-  const double phase_ratio = static_cast<double>(kMaxPhase - phase) / kMaxPhase;
-
-  double tapered_eval =
-      Lerp(score_pair.MiddleGame(), score_pair.EndGame(), phase_ratio);
-
-  constexpr Score kTempoBonus = 10;
-  return static_cast<Score>(tapered_eval + kTempoBonus);
+  const int kTempoBonus = 10;
+  return EvaluateMaterial(state) + kTempoBonus;
 }
 
 }  // namespace eval
