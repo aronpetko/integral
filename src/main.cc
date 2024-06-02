96f25e1b70c8faeb851f5273d810d3bbc6051428
Aron Petkovski <aronpetko@gmail.com>
2024-06-02 14:58:17 -0500

    Disabled stdout buffering

--- src/main.cc
+++ src/main.cc
@@ -10,6 +10,9 @@ int main(int arg_count, char **args) {
   SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE),
                  ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 #endif
-  
+
+  // Disable buffering to stdout
+  setbuf(stdout, nullptr);
+
   uci::AcceptCommands(arg_count, args);
 }
