bb0eab5653073a6d5cfeb715224f8c9874db4899
Aron Petkovski <aronpetko@gmail.com>
2024-06-24 14:41:00 -0500

    boy

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -194,6 +194,13 @@ Score Search::QuiescentSearch(Score alpha,
   MovePicker move_picker(
       MovePickerType::kQuiescence, board_, tt_move, history_, stack);
   while (const auto move = move_picker.Next()) {
+    // Stop searching since all the good tactical moves have been searched,
+    // unless we need to find a quiet evasion
+    if (move_picker.GetStage() > MovePicker::Stage::kGoodTacticals &&
+        moves_seen > 0) {
+      break;
+    }
+    
     if (!board_.IsMoveLegal(move)) {
       continue;
     }
