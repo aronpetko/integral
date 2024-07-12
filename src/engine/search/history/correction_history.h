a7982c83033fd00ef8d2d4b97d56e78b16204346
Aron Petkovski <aronpetko@gmail.com>
2024-07-11 19:05:24 -0500

    Make search parameters tunable
    Bench: 3380203

--- src/engine/search/history/correction_history.h
+++ src/engine/search/history/correction_history.h
@@ -8,8 +8,6 @@
 namespace history {
 
 inline Tunable corr_history_size("corr_history_size", 16384, 8192, 32768, 1024);
-inline Tunable corr_history_max_bonus(
-    "corr_history_max_bonus", 128, 64, 256, 8);
 inline Tunable corr_history_gravity("corr_history_gravity", 512, 256, 1024, 32);
 
 class CorrectionHistory {
@@ -39,7 +37,8 @@ class CorrectionHistory {
     const Score static_eval = eval::Evaluate(state_);
     const Score correction = table_[state_.turn][GetTableIndex()];
     const Score adjusted_score =
-        static_eval + (correction * std::abs(correction)) / corr_history_size;
+        static_eval + (correction * std::abs(correction)) /
+                          static_cast<int>(corr_history_size);
 
     // Ensure no static evaluations are mate scores
     return std::clamp(adjusted_score,
