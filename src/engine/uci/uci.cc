277bdacfa2d84eefc3a987d4425b6a368f723fca
Aron Petkovski <aronpetko@gmail.com>
2024-07-13 02:03:56 -0500

    Bench: 3380203

--- src/engine/uci/uci.cc
+++ src/engine/uci/uci.cc
@@ -110,11 +110,9 @@ void Initialize(Board &board, Search &search) {
 
   listener.RegisterCommand("stop", CommandType::kUnordered, {}, [&search](Command *cmd) {
     search.Stop();
-    search.Wait();
   });
 
   listener.RegisterCommand("ucinewgame", CommandType::kUnordered, {}, [&search](Command *cmd) {
-    search.Stop();
     search.Wait();
     search.NewGame();
   });
