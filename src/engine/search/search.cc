8da2a5bf5d48b1de9c5b2851f99747cb904f8c75
Aron Petkovski <aronpetko@gmail.com>
2024-07-13 02:22:58 -0500

    Bench: 3380203

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -68,9 +68,10 @@ void Search::Run() {
       searching_.store(true, std::memory_order_seq_cst);
 
       if (benching_.load(std::memory_order_relaxed)) {
-        IterativeDeepening<SearchType::kBench>();
+        // IterativeDeepening<SearchType::kBench>();
       } else {
-        IterativeDeepening<SearchType::kRegular>();
+        fmt::println("yep");
+        // IterativeDeepening<SearchType::kRegular>();
       }
 
       Stop();
@@ -92,7 +93,7 @@ void Search::Start(TimeConfig &time_config) {
     time_mgmt_.Start();
   }
   nodes_searched_.store(0, std::memory_order_seq_cst);
-  //start_search_.store(true, std::memory_order_seq_cst);
+  start_search_.store(true, std::memory_order_seq_cst);
   stopped_.store(false, std::memory_order_seq_cst);
 }
 
