8f9a918e03e67e75344b97af69aa3eacdb6c56c1
Aron Petkovski <aronpetko@gmail.com>
2024-07-15 10:30:26 -0500

    Bench: 3562872

--- src/engine/search/constants.h
+++ src/engine/search/constants.h
@@ -3,47 +3,47 @@
 
 #include "../../tuner/spsa.h"
 
-Tunable lmr_quiet_base("lmr_quiet_base", 0.79, 0.5, 1.2, 0.05);
-Tunable lmr_quiet_div("lmr_quiet_div", 1.98, 1.5, 3.0, 0.1);
-Tunable lmr_tact_base("lmr_tact_base", -0.27, -0.5, 0.2, 0.05);
+Tunable lmr_quiet_base("lmr_quiet_base", 0.80, 0.5, 1.2, 0.05);
+Tunable lmr_quiet_div("lmr_quiet_div", 2.02, 1.5, 3.0, 0.1);
+Tunable lmr_tact_base("lmr_tact_base", -0.23, -0.5, 0.2, 0.05);
 Tunable lmr_tact_div("lmr_tact_div", 2.61, 2.0, 3.5, 0.1);
 
 Tunable asp_window_depth("asp_window_depth", 4, 2, 8, 1, true);
-Tunable asp_window_delta("asp_window_delta", 9, 5, 20, 2);
-Tunable asp_window_growth("asp_window_growth", 1.57, 0.1, 1.0, 0.05);
+Tunable asp_window_delta("asp_window_delta", 10, 5, 20, 2);
+Tunable asp_window_growth("asp_window_growth", 1.58, 0.1, 1.0, 0.05);
 
-Tunable qs_fut_margin("qs_fut_margin", 99, 50, 200, 10);
+Tunable qs_fut_margin("qs_fut_margin", 101, 50, 200, 10);
 
 Tunable rev_fut_depth("rev_fut_depth", 6, 4, 10, 1, true);
 Tunable rev_fut_margin("rev_fut_margin", 75, 50, 150, 8);
 
 Tunable null_move_rb("null_move_rb", 3, 1, 5, 0.5);
 Tunable null_move_rf("null_move_rf", 4, 2, 8, 1, true);
-Tunable null_move_re("null_move_re", 195, 100, 400, 20);
+Tunable null_move_re("null_move_re", 203, 100, 400, 20);
 
 Tunable iir_depth("iir_depth", 4, 2, 8, 1);
 
-Tunable improving_rate_divisor("improving_rate_divisor", 22.0, 5.0, 100.0, 15.0);
+Tunable improving_rate_divisor("improving_rate_divisor", 31.0, 5.0, 100.0, 15.0);
 
-Tunable lmp_base("lmp_base", 4.3, 1.5, 5.0, 0.5);
-Tunable lmp_mult("lmp_mult", 2.543, 1.0, 4.0, 0.25);
+Tunable lmp_base("lmp_base", 5.0, 1.5, 5.0, 0.5);
+Tunable lmp_mult("lmp_mult", 2.51, 1.0, 4.0, 0.25);
 
 Tunable fut_prune_depth("fut_prune_depth", 8, 6, 12, 1, true);
-Tunable fut_margin_base("fut_margin_base", 185, 100, 250, 20);
-Tunable fut_margin_mult("fut_margin_mult", 93, 50, 200, 15);
+Tunable fut_margin_base("fut_margin_base", 190, 100, 250, 20);
+Tunable fut_margin_mult("fut_margin_mult", 96, 50, 200, 15);
 
 Tunable see_prune_depth("see_prune_depth", 8, 6, 12, 1, true);
-Tunable see_quiet_thresh("see_quiet_thresh", -62, -100, -20, 6);
-Tunable see_noisy_thresh("see_noisy_thresh", -124, -200, -50, 8);
+Tunable see_quiet_thresh("see_quiet_thresh", -65, -100, -20, 6);
+Tunable see_noisy_thresh("see_noisy_thresh", -120, -200, -50, 8);
 
 Tunable hist_prune_depth("hist_prune_depth", 5, 3, 8, 1, true);
-Tunable hist_thresh_base("hist_thresh_base", -482, -1000, 500, 100);
-Tunable hist_thresh_mult("hist_thresh_mult", -1687, -3000, -250, 300);
+Tunable hist_thresh_base("hist_thresh_base", -521, -1000, 500, 100);
+Tunable hist_thresh_mult("hist_thresh_mult", -1742, -3000, -250, 300);
 
-Tunable lmr_hist_div("lmr_hist_div", 11253, 5000, 20000, 750);
+Tunable lmr_hist_div("lmr_hist_div", 11525, 5000, 20000, 750);
 
 Tunable sing_ext_depth("sing_ext_depth", 8, 6, 12, 1, true);
 Tunable sing_ext_margin("sing_ext_margin", 2.28, 1.0, 4.0, 0.5);
-Tunable sing_double_margin("sing_double_margin", 28, 10, 40, 5);
+Tunable sing_double_margin("sing_double_margin", 27, 10, 40, 5);
  
 #endif  // SPRT_SH_CONSTANTS_H
