851a96f1205cacdeba793eaf3bc69552c1898d27
Aron Petkovski <aronpetko@gmail.com>
2024-07-17 16:30:58 -0500

    bench 2654473

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -588,7 +588,7 @@ Score Search::PVSearch(int depth,
       reduction -= is_quiet * history_.GetQuietMoveScore(move, stack) /
                    static_cast<int>(lmr_hist_div);
       reduction -= state.InCheck();
-      reduction += !improving;
+      reduction -= improving;
 
       // Ensure the reduction doesn't give us a depth below 0
       reduction = std::clamp<int>(reduction, 0, new_depth - 1);
