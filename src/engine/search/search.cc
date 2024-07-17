919bdac986f444cf5fb34b89c9d08848cbef5108
Aron Petkovski <aronpetko@gmail.com>
2024-07-16 19:51:02 -0500

    Bench: 2618687

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -390,7 +390,9 @@ Score Search::PVSearch(int depth,
     // Reverse (Static) Futility Pruning: Cutoff if we think the position can't
     // fall below beta anytime soon
     if (depth <= rev_fut_depth && eval < kMateScore - kMaxPlyFromRoot) {
-      const int futility_margin = depth * rev_fut_margin;
+      const int futility_margin =
+          depth * rev_fut_margin -
+          static_cast<int>(60 * std::max(0.0, stack->improving_rate));
       if (eval - futility_margin >= beta) {
         return eval;
       }
