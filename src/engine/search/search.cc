671469655478ad029b92984e6a76732bdff3d98e
Aron Petkovski <aronpetko@gmail.com>
2024-07-18 01:20:25 -0500

    Bench: 2004841

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -396,7 +396,7 @@ Score Search::PVSearch(int depth,
     if (depth <= rev_fut_depth && stack->eval < kMateScore - kMaxPlyFromRoot) {
       const int futility_margin =
           static_cast<int>(depth * rev_fut_margin /
-                           (1 + std::clamp(stack->improving_rate, 0.0, 0.65)));
+                           (1 + std::clamp(stack->improving_rate, 0.0, 0.85)));
       if (stack->eval - futility_margin >= beta) {
         return stack->eval;
       }
