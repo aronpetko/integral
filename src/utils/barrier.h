a58b8f47f36b2bccabf9e33d4e29af23072caeea
Aron Petkovski <aronpetko@gmail.com>
2024-07-29 00:44:08 -0500

    Bench: 2999049

--- src/utils/barrier.h
+++ src/utils/barrier.h
@@ -4,10 +4,11 @@
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
+#include <cassert>
+#include <atomic>
 
 #include "types.h"
 
-// Credits to Stormphrax
 class Barrier {
  public:
   explicit Barrier(I64 expected) : total_(0), current_(0), phase_(0) {
@@ -16,20 +17,21 @@ class Barrier {
 
   void Reset(I64 expected) {
     assert(expected > 0);
-    assert(current_.load() == total.load());
+    std::lock_guard<std::mutex> lock(wait_mutex_);
+    assert(current_.load(std::memory_order::acquire) == total_.load(std::memory_order::acquire));
 
     total_.store(expected, std::memory_order::seq_cst);
     current_.store(expected, std::memory_order::seq_cst);
   }
 
   void ArriveAndWait() {
-    std::unique_lock lock(wait_mutex_);
+    std::unique_lock<std::mutex> lock(wait_mutex_);
 
     const I64 current = --current_;
     if (current > 0) {
       const auto phase = phase_.load(std::memory_order::relaxed);
       wait_signal_.wait(lock, [this, phase] {
-        return (phase - phase_.load(std::memory_order::acquire)) < 0;
+        return (phase - phase_.load(std::memory_order::acquire)) != 0;
       });
     } else {
       const auto total = total_.load(std::memory_order::acquire);
