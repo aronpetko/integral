a4316ef36157bc7ee6dc1908128f5b4762b7a94a
Aron Petkovski <aronpetko@gmail.com>
2024-07-30 21:32:50 -0500

    Bench: 2774583

---
+++ build/CMakeFiles/integral.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Coding/integral/third-party/fathom/tbprobe.c" "CMakeFiles/integral.dir/third-party/fathom/tbprobe.c.obj" "gcc" "CMakeFiles/integral.dir/third-party/fathom/tbprobe.c.obj.d"
+  "D:/Coding/integral/src/chess/board.cc" "CMakeFiles/integral.dir/src/chess/board.cc.obj" "gcc" "CMakeFiles/integral.dir/src/chess/board.cc.obj.d"
+  "D:/Coding/integral/src/chess/fen.cc" "CMakeFiles/integral.dir/src/chess/fen.cc.obj" "gcc" "CMakeFiles/integral.dir/src/chess/fen.cc.obj.d"
+  "D:/Coding/integral/src/chess/move.cc" "CMakeFiles/integral.dir/src/chess/move.cc.obj" "gcc" "CMakeFiles/integral.dir/src/chess/move.cc.obj.d"
+  "D:/Coding/integral/src/chess/move_gen.cc" "CMakeFiles/integral.dir/src/chess/move_gen.cc.obj" "gcc" "CMakeFiles/integral.dir/src/chess/move_gen.cc.obj.d"
+  "D:/Coding/integral/src/engine/evaluation/evaluation.cc" "CMakeFiles/integral.dir/src/engine/evaluation/evaluation.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/evaluation/evaluation.cc.obj.d"
+  "D:/Coding/integral/src/engine/search/move_picker.cc" "CMakeFiles/integral.dir/src/engine/search/move_picker.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/search/move_picker.cc.obj.d"
+  "D:/Coding/integral/src/engine/search/search.cc" "CMakeFiles/integral.dir/src/engine/search/search.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/search/search.cc.obj.d"
+  "D:/Coding/integral/src/engine/search/syzygy/syzygy.cc" "CMakeFiles/integral.dir/src/engine/search/syzygy/syzygy.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/search/syzygy/syzygy.cc.obj.d"
+  "D:/Coding/integral/src/engine/search/time_mgmt.cc" "CMakeFiles/integral.dir/src/engine/search/time_mgmt.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/search/time_mgmt.cc.obj.d"
+  "D:/Coding/integral/src/engine/search/transpo.cc" "CMakeFiles/integral.dir/src/engine/search/transpo.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/search/transpo.cc.obj.d"
+  "D:/Coding/integral/src/engine/uci/uci.cc" "CMakeFiles/integral.dir/src/engine/uci/uci.cc.obj" "gcc" "CMakeFiles/integral.dir/src/engine/uci/uci.cc.obj.d"
+  "D:/Coding/integral/src/magics/attacks.cc" "CMakeFiles/integral.dir/src/magics/attacks.cc.obj" "gcc" "CMakeFiles/integral.dir/src/magics/attacks.cc.obj.d"
+  "D:/Coding/integral/src/magics/magic_finder.cc" "CMakeFiles/integral.dir/src/magics/magic_finder.cc.obj" "gcc" "CMakeFiles/integral.dir/src/magics/magic_finder.cc.obj.d"
+  "D:/Coding/integral/src/main.cc" "CMakeFiles/integral.dir/src/main.cc.obj" "gcc" "CMakeFiles/integral.dir/src/main.cc.obj.d"
+  "D:/Coding/integral/src/tests/bench_test.cc" "CMakeFiles/integral.dir/src/tests/bench_test.cc.obj" "gcc" "CMakeFiles/integral.dir/src/tests/bench_test.cc.obj.d"
+  "D:/Coding/integral/src/tests/perft_test.cc" "CMakeFiles/integral.dir/src/tests/perft_test.cc.obj" "gcc" "CMakeFiles/integral.dir/src/tests/perft_test.cc.obj.d"
+  "D:/Coding/integral/src/tests/see_test.cc" "CMakeFiles/integral.dir/src/tests/see_test.cc.obj" "gcc" "CMakeFiles/integral.dir/src/tests/see_test.cc.obj.d"
+  "D:/Coding/integral/src/tuner/tuner.cc" "CMakeFiles/integral.dir/src/tuner/tuner.cc.obj" "gcc" "CMakeFiles/integral.dir/src/tuner/tuner.cc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
