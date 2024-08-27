d999fa749462389bec70757edf9aecf986bbfbfd
Aron Petkovski <aronpetko@gmail.com>
2024-08-27 02:08:46 -0500

    Bench: 3029274

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -552,7 +552,7 @@ Score Search::PVSearch(Thread &thread,
     // Razoring: At low depths, if this node seems like it might fail low, we do
     // a quiescent search to determine if we should prune
     if (!stack->excluded_tt_move && depth <= 4 &&
-        stack->eval + 300 * depth <= alpha) {
+        stack->eval + 450 * depth < alpha) {
       const Score razoring_score =
           QuiescentSearch<NodeType::kNonPV>(thread, alpha, alpha + 1, stack);
       if (razoring_score <= alpha) {
