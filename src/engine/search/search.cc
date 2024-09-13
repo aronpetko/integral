f8c92205109342c0d52f060157ad486933bb5d4f
Aron Petkovski <aronpetko@gmail.com>
2024-09-13 01:06:56 -0500

    Bench: 2258612

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -670,7 +670,7 @@ Score Search::PVSearch(Thread &thread,
   // Internal Iterative Reduction: Move ordering is expected to be worse with no
   // TT move, so we save time on searching this position now
   if ((in_pv_node || cut_node) && depth >= iir_depth &&
-      !stack->excluded_tt_move && !tt_move) {
+      !stack->excluded_tt_move && (!tt_move || tt_entry->depth + 4 < depth)) {
     depth--;
   }
 
