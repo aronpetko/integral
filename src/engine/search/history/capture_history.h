41475f2c8120e0c5a4a49008f55fe1172a1e31d1
Aron Petkovski <aronpetko@gmail.com>
2024-09-24 16:10:18 -0500

    Bench: 2135254

--- src/engine/search/history/capture_history.h
+++ src/engine/search/history/capture_history.h
@@ -11,13 +11,14 @@ class CaptureHistory {
  public:
   CaptureHistory() : table_({}) {}
 
-  void UpdateScore(const BoardState &state,
-                   StackEntry *stack,
-                   int depth) {
+  void UpdateScore(const BoardState &state, StackEntry *stack, int depth) {
     const int bonus = HistoryBonus(depth);
+    const auto moving_piece = state.GetPieceType(stack->move.GetFrom());
+    const auto captured_piece = state.GetPieceType(stack->move.GetTo());
     // Apply a linear dampening to the bonus as the depth increases
-    int &score = table_[state.turn][stack->move.GetFrom()][stack->move.GetTo()]
-                       [ThreatIndex(stack->move, stack->threats)];
+    int &score =
+        table_[state.turn][moving_piece][captured_piece][stack->move.GetTo()]
+              [ThreatIndex(stack->move, stack->threats)];
     score += ScaleBonus(score, bonus);
   }
 
@@ -29,9 +30,11 @@ class CaptureHistory {
     // Lower the score of the capture moves that failed to raise alpha
     for (int i = 0; i < captures.Size(); i++) {
       const Move bad_capture = captures[i];
+      const auto moving_piece = state.GetPieceType(bad_capture.GetFrom());
+      const auto captured_piece = state.GetPieceType(bad_capture.GetTo());
       // Apply a linear dampening to the penalty as the depth increases
       int &bad_capture_score =
-          table_[state.turn][bad_capture.GetFrom()][bad_capture.GetTo()]
+          table_[state.turn][moving_piece][captured_piece][bad_capture.GetTo()]
                 [ThreatIndex(bad_capture, threats)];
       bad_capture_score += ScaleBonus(bad_capture_score, -bonus);
     }
@@ -40,7 +43,9 @@ class CaptureHistory {
   [[nodiscard]] int GetScore(const BoardState &state,
                              Move move,
                              BitBoard threats) const {
-    return table_[state.turn][move.GetFrom()][move.GetTo()]
+    const auto moving_piece = state.GetPieceType(move.GetFrom());
+    const auto captured_piece = state.GetPieceType(move.GetTo());
+    return table_[state.turn][moving_piece][captured_piece][move.GetTo()]
                  [ThreatIndex(move, threats)];
   }
 
@@ -50,7 +55,8 @@ class CaptureHistory {
   }
 
  private:
-  MultiArray<int, kNumColors, kSquareCount, kSquareCount, 4> table_;
+  MultiArray<int, kNumColors, kNumPieceTypes, kNumPieceTypes, kSquareCount, 4>
+      table_;
 };
 
 }  // namespace search::history
