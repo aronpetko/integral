5a96c19c61ef39225155231b82ca3bd3cc090e39
Aron Petkovski <aronpetko@gmail.com>
2024-09-26 14:13:34 -0500

    Bench: 1837794

--- src/engine/search/move_picker.cc
+++ src/engine/search/move_picker.cc
@@ -46,7 +46,7 @@ Move MovePicker::Next() {
 
       const bool loses_material = !eval::StaticExchange(
           move,
-          type_ != MovePickerType::kNoisy ? -history_score / 4 : see_threshold_,
+          type_ != MovePickerType::kNoisy ? -history_score / 32 : see_threshold_,
           state);
       if (!loses_material && !move.IsUnderPromotion()) {
         return move;
