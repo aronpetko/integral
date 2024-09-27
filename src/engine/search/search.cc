d048e1cfb6278eca6288add08ffbf4f12aba7383
Aron Petkovski <aronpetko@gmail.com>
2024-09-27 11:03:42 -0500

    Bench: 1681264

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -744,7 +744,8 @@ Score Search::PVSearch(Thread &thread,
       // Static Exchange Evaluation (SEE) Pruning: Skip moves that lose too much
       // material
       const int see_threshold =
-          is_quiet ? see_quiet_thresh * depth : see_noisy_thresh * depth;
+          is_quiet ? see_quiet_thresh * depth
+                   : see_noisy_thresh * depth - stack->history_score / 150;
       if (depth <= see_prune_depth && moves_seen >= 1 &&
           !eval::StaticExchange(move, see_threshold, state)) {
         continue;
