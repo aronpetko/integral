c25d677ddb91eeddd7b1a24560a1fa54c6a0c071
Aron Petkovski <aronpetko@gmail.com>
2024-10-23 17:03:17 -0500

    Fix stuff

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -948,14 +948,15 @@ Score Search::PVSearch(Thread &thread,
 
         alpha = score;
         if (alpha >= beta) {
+          const int history_depth = depth + (alpha > beta + 50);
           if (is_quiet) {
             stack->AddKillerMove(move);
             history.quiet_history->UpdateScore(
-                state, stack, depth, stack->threats, quiets);
+                state, stack, history_depth, stack->threats, quiets);
             history.continuation_history->UpdateScore(
-                state, stack, depth, quiets);
+                state, stack, history_depth, quiets);
           } else if (is_capture) {
-            history.capture_history->UpdateScore(state, stack, depth);
+            history.capture_history->UpdateScore(state, stack, history_depth);
           }
           // Beta cutoff: The opponent had a better move earlier in the tree
           break;
