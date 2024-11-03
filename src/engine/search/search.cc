b4f64dac1b7f7b4e7884ee63902e3acd00514513
Aron Petkovski <aronpetko@gmail.com>
2024-11-02 21:08:53 -0500

    Bench: 1902427

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -608,7 +608,7 @@ Score Search::PVSearch(Thread &thread,
     // Razoring: At low depths, if this node seems like it might fail low, we do
     // a quiescent search to determine if we should prune
     if (!stack->excluded_tt_move && depth <= kRazoringDepth &&
-        stack->static_eval + kRazoringMult * (depth - !improving) < alpha) {
+        stack->eval + kRazoringMult * (depth - !improving) < alpha) {
       const Score razoring_score =
           QuiescentSearch<NodeType::kNonPV>(thread, alpha, alpha + 1, stack);
       if (razoring_score <= alpha) {
