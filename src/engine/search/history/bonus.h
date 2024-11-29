cfa57faba23cf937210c0498226bae12ec569ca7
Aron Petkovski <aronpetko@gmail.com>
2024-11-29 14:44:38 -0600

    Bench: 1984867

--- src/engine/search/history/bonus.h
+++ src/engine/search/history/bonus.h
@@ -7,10 +7,10 @@
 namespace search::history {
 
 TUNABLE(kHistBonusGravity, 10979, 8192, 32768, false);
-TUNABLE(kHistBonusScale, 147, 65, 260, false);
-TUNABLE(kHistPenaltyScale, 160, 65, 260, false);
+TUNABLE(kHistBonusScale, 137, 65, 260, false);
+TUNABLE(kHistPenaltyScale, 150, 65, 260, false);
 TUNABLE(kHistBonusMaxBonus, 1188, 580, 2318, true);
-TUNABLE(kHistBonusBias, 100, 580, 2318, true);
+TUNABLE(kHistBonusBias, 200, 580, 2318, true);
 
 static I16 HistoryBonus(I16 depth,
                         I16 scale = kHistBonusScale,
@@ -22,7 +22,7 @@ static I16 HistoryPenalty(I16 depth,
                           I16 scale = kHistPenaltyScale,
                           I16 max_bonus = kHistBonusMaxBonus) {
   return std::clamp<I16>(
-      -scale * depth - kHistBonusBias, -max_bonus, max_bonus);
+      -scale * depth - kHistBonusBiasbw, -max_bonus, max_bonus);
 }
 
 // Linear interpolation of the bonus and maximum score
