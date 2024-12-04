11ae67f6d35e8c8a8d64b3fd24a8025cd11b27c2
Aron Petkovski <aronpetko@gmail.com>
2024-12-04 12:59:36 -0600

    Bench: 1853872

--- src/engine/search/history/capture_history.h
+++ src/engine/search/history/capture_history.h
@@ -13,8 +13,11 @@ class CaptureHistory {
 
   void UpdateScore(const BoardState &state, StackEntry *stack, I16 depth) {
     const I16 bonus = HistoryBonus(depth);
+    const auto from = stack->move.GetFrom(), to = stack->move.GetTo();
+    const auto captured =
+        stack->move.IsEnPassant(state) ? kPawn : state.GetPieceType(to);
     // Apply a linear dampening to the bonus as the depth increases
-    I16 &score = table_[state.turn][stack->move.GetFrom()][stack->move.GetTo()];
+    I16 &score = table_[state.turn][state.GetPieceType(from)][to][captured];
     score += ScaleBonus(score, bonus);
   }
 
@@ -23,19 +26,26 @@ class CaptureHistory {
     // Lower the score of the capture moves that failed to raise alpha
     for (I16 i = 0; i < captures.Size(); i++) {
       const Move bad_capture = captures[i];
+      const auto from = bad_capture.GetFrom(), to = bad_capture.GetTo();
+      const auto captured =
+          bad_capture.IsEnPassant(state) ? kPawn : state.GetPieceType(to);
       // Apply a linear dampening to the penalty as the depth increases
       I16 &bad_capture_score =
-          table_[state.turn][bad_capture.GetFrom()][bad_capture.GetTo()];
+          table_[state.turn][state.GetPieceType(from)][to][captured];
       bad_capture_score += ScaleBonus(bad_capture_score, penalty);
     }
   }
 
   [[nodiscard]] I16 GetScore(const BoardState &state, Move move) const {
-    return table_[state.turn][move.GetFrom()][move.GetTo()];
+    const auto from = move.GetFrom(), to = move.GetTo();
+    const auto captured =
+        move.IsEnPassant(state) ? kPawn : state.GetPieceType(to);
+    return table_[state.turn][state.GetPieceType(from)][to][captured];
   }
 
  private:
-  MultiArray<I16, kNumColors, kSquareCount, kSquareCount> table_;
+  MultiArray<I16, kNumColors, kNumPieceTypes, kSquareCount, kNumPieceTypes>
+      table_;
 };
 
 }  // namespace search::history
