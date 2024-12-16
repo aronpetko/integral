723ec06a3a859f5fa15e9980982306c7493fb192
aronpetko <aronpetko@gmail.com>
2024-12-16 11:59:07 -0600

    Bench: 1978811

--- src/engine/evaluation/evaluation.cc
+++ src/engine/evaluation/evaluation.cc
@@ -18,7 +18,7 @@ Score Evaluate(Board &board) {
   network_eval = network_eval * (26500 + material_phase) / 32768;
 
   // Scale based on proximity to a 50 move rule draw
-  network_eval -= network_eval * state.fifty_moves_clock / 230;
+  network_eval = network_eval * (230 - state.fifty_moves_clock) / 230;
 
   return std::clamp(
       network_eval, -kTBWinInMaxPlyScore + 1, kTBWinInMaxPlyScore - 1);
