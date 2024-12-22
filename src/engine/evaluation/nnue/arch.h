d5bd3ecde86f46fd64762244de1967dfd3e984f3
aronpetko <aronpetko@gmail.com>
2024-12-21 19:53:10 -0600

    Bench: 1810018

--- src/engine/evaluation/nnue/arch.h
+++ src/engine/evaluation/nnue/arch.h
@@ -12,7 +12,7 @@ constexpr std::size_t kOutputBucketCount = 8;
 constexpr std::int32_t kHiddenLayerQuantization = 255;
 constexpr std::int32_t kOutputQuantization = 64;
 
-constexpr std::int32_t kEvalScale = 400;
+constexpr std::int32_t kEvalScale = 300;
 
 };  // namespace nnue::arch
 
