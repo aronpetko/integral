224a65b248d7505e580d3931dd26abd9b7e43dad
aronpetko <aronpetko@gmail.com>
2025-01-11 13:40:00 -0600

    Bench: 1749369

--- src/engine/search/time_mgmt.cc
+++ src/engine/search/time_mgmt.cc
@@ -5,16 +5,16 @@
 
 namespace search {
 
-TUNABLE(kStabilityBase, 1.3258171143194901, 0.5, 2.0, false);
-TUNABLE(kStabilitySlope, 0.05701240506450806, 0.0, 0.25, false);
-TUNABLE(kScoreChangeBase, 0.11583788523947108, 0.0, 0.2, false);
-TUNABLE(kSearchScoreCoeff, 0.024976017482516696, 0.0, 0.05, false);
-TUNABLE(kPreviousScoreCoeff, 0.02417149548721014, 0.0, 0.05, false);
-TUNABLE(kScoreChangeMin, 0.5065463867970877, 0.3, 0.7, false);
-TUNABLE(kScoreChangeMax, 1.6428181385696197, 1.25, 2.0, false);
-TUNABLE(kNodeFactorBase, 0.5557521778517045, 0.3, 0.7, false);
-TUNABLE(kNodeFactorSlope, 2.2448900261513955, 1.8, 3.0, false);
-TUNABLE(kNodeFactorIntercept, 0.44534251191329405, 0.2, 0.65, false);
+TUNABLE(kStabilityBase, 1.3016343835452844, 0.5, 2.0, false);
+TUNABLE(kStabilitySlope, 0.05598465466016829, 0.0, 0.25, false);
+TUNABLE(kScoreChangeBase, 0.10899333443947289, 0.0, 0.2, false);
+TUNABLE(kSearchScoreCoeff, 0.024781680887691854, 0.0, 0.05, false);
+TUNABLE(kPreviousScoreCoeff, 0.023955545789994755, 0.0, 0.05, false);
+TUNABLE(kScoreChangeMin, 0.5073325256823985, 0.3, 0.7, false);
+TUNABLE(kScoreChangeMax, 1.6525072044919396, 1.25, 2.0, false);
+TUNABLE(kNodeFactorBase, 0.5497120909391846, 0.3, 0.7, false);
+TUNABLE(kNodeFactorSlope, 2.2776174563428637, 1.8, 3.0, false);
+TUNABLE(kNodeFactorIntercept, 0.447321836576184, 0.2, 0.65, false);
 
 U64 GetCurrentTime() {
   const auto duration = std::chrono::steady_clock::now().time_since_epoch();
