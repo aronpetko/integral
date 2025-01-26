43091c695d9221407000ad69b9b8ada893281ac8
aronpetko <aronpetko@gmail.com>
2025-01-25 18:32:17 -0600

    Bench: 2421955

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -703,7 +703,7 @@ Score Search::PVSearch(Thread &thread,
 
     // Null Move Pruning: Forfeit a move to our opponent and cutoff if we
     // still have the advantage
-    if ((cut_node || stack->eval >= beta) && !(stack - 1)->move.IsNull() &&
+    if (!(stack - 1)->move.IsNull() && cut_node && stack->eval >= beta &&
         stack->static_eval >= beta + kNmpBetaBase - kNmpBetaMult * depth &&
         !stack->excluded_tt_move && stack->ply >= thread.nmp_min_ply) {
       // Avoid null move pruning a position with high zugzwang potential
