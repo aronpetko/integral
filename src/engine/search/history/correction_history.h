5264f8aa0dfc7ffde0fb948b78d78236d91f883c
aronpetko <aronpetko@gmail.com>
2025-01-31 22:50:17 -0600

    Bench: 2739677

--- src/engine/search/history/correction_history.h
+++ src/engine/search/history/correction_history.h
@@ -10,7 +10,7 @@ namespace search::history {
 TUNABLE_STEP(kPawnCorrectionWeight, 43, 0, 125, false, 8);
 TUNABLE_STEP(kNonPawnCorrectionWeight, 39, 0, 125, false, 8);
 TUNABLE_STEP(kMajorCorrectionWeight, 42, 0, 125, false, 8);
-TUNABLE_STEP(kContinuationCorrectionWeight, 45, 0, 125, false, 8);
+TUNABLE_STEP(kContinuationCorrectionWeight, 51, 0, 125, false, 8);
 
 class CorrectionHistory {
  public:
