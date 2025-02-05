ba1d7358f498f108da2d3b9d29d0b45ab0c799ab
Aron Petkovski <aronpetko@gmail.com>
2025-02-05 11:59:41 -0600

    Bench 2377566

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -637,7 +637,7 @@ Score Search::PVSearch(Thread &thread,
 
   const auto &prev_stack = stack - 1;
   if (stack->ply > 1 && prev_stack->move && !prev_stack->capture_move &&
-      !prev_stack->in_check) {
+      !prev_stack->in_check && !stack->in_check) {
     const int bonus =
         std::clamp<int>(-kEvalHistUpdateMult *
                             (stack->static_eval + prev_stack->static_eval) / 10,
