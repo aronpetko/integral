21eaf7363db6a1aa8427d30d2adb7e0c8cdbadd2
Aron Petkovski <aronpetko@gmail.com>
2025-02-05 15:15:40 -0600

    Bench: 2363763

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -347,7 +347,7 @@ Score Search::QuiescentSearch(Thread &thread,
       continue;
     }
 
-    if (best_score > -kTBWinInMaxPlyScore &&
+    if (best_score > -kTBWinInMaxPlyScore && !stack->in_check &&
         (move.IsUnderPromotion() || move.GetType() != MoveType::kPromotion)) {
       if (moves_seen >= 2) {
         break;
@@ -355,8 +355,8 @@ Score Search::QuiescentSearch(Thread &thread,
 
       // QS Futility Pruning: Prune capture moves that don't win material if the
       // static eval is behind alpha by some margin
-      if (!stack->in_check && move.IsCapture(state) &&
-          futility_score <= alpha && !eval::StaticExchange(move, 1, state)) {
+      if (move.IsCapture(state) && futility_score <= alpha &&
+          !eval::StaticExchange(move, 1, state)) {
         best_score = std::max(best_score, futility_score);
         continue;
       }
