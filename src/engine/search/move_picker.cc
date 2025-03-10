4b0c066aaeddfc42722b7de730a31cecd6be16fc
aronpetkovski <aronpetko@gmail.com>
2025-03-10 17:03:33 -0500

    Bench: 2627823

--- src/engine/search/move_picker.cc
+++ src/engine/search/move_picker.cc
@@ -243,7 +243,7 @@ int MovePicker::ScoreMove(Move &move) {
   // The higher the depth this move caused a cutoff the more likely it move will
   // be ordered first
   return threat_score +
-         board_.MoveGivesCheck(move) * 16384 +
+         board_.MoveGivesCheck(move) * 8192 +
          history_.GetQuietMoveScore(state, move, state.threats, stack_);
 }
 
