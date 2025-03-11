9f46b19b417dcb9c0ab626ec47b4ecf42dcf5bf7
aronpetko <aronpetko@gmail.com>
2025-03-10 22:56:02 -0500

    Bench: 2530026

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -919,8 +919,7 @@ Score Search::PVSearch(Thread &thread,
           kFutMarginMult * lmr_fractional_depth / kLmrDepthScale +
           stack->history_score / kFutMarginHistDiv;
       if (lmr_depth <= kFutPruneDepth && !stack->in_check && is_quiet &&
-          stack->static_eval + futility_margin < alpha &&
-          !board.MoveGivesCheck(move)) {
+          stack->static_eval + futility_margin < alpha) {
         move_picker.SkipQuiets();
         continue;
       }
