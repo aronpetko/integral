2acc3d818d16b87811937956bb05644f53e61470
Shawn Xu <xu107288696@gmail.com>
2025-03-25 01:59:34 -0500

    cleanup

--- src/engine/evaluation/nnue/accumulator.h
+++ src/engine/evaluation/nnue/accumulator.h
@@ -76,11 +76,11 @@ class PerspectiveAccumulator {
     const Square king_square = state.King(perspective).GetLsb();
     for (int piece = PieceType::kPawn; piece <= PieceType::kKing; ++piece) {
       for (Square square : state.piece_bbs[piece]) {
-        ApplyChange<Add>(perspective,
-                         king_square,
-                         FeatureData{square,
-                                     static_cast<PieceType>(piece),
-                                     state.GetPieceColor(square)});
+        ApplyChange<kAdd>(perspective,
+                          king_square,
+                          FeatureData{square,
+                                      static_cast<PieceType>(piece),
+                                      state.GetPieceColor(square)});
       }
     }
   }
@@ -145,25 +145,25 @@ class PerspectiveAccumulator {
                    Square king_square) {
     switch (change.type) {
       case AccumulatorChange::kNormal:
-        ApplyChange<Add, Sub>(
+        ApplyChange<kAdd, kSub>(
             previous, perspective, king_square, change.add_0, change.sub_0);
         break;
       case AccumulatorChange::kCapture:
-        ApplyChange<Add, Sub, Sub>(previous,
-                                   perspective,
-                                   king_square,
-                                   change.add_0,
-                                   change.sub_0,
-                                   change.sub_1);
+        ApplyChange<kAdd, kSub, kSub>(previous,
+                                      perspective,
+                                      king_square,
+                                      change.add_0,
+                                      change.sub_0,
+                                      change.sub_1);
         break;
       case AccumulatorChange::kCastle:
-        ApplyChange<Add, Add, Sub, Sub>(previous,
-                                        perspective,
-                                        king_square,
-                                        change.add_0,
-                                        change.add_1,
-                                        change.sub_0,
-                                        change.sub_1);
+        ApplyChange<kAdd, kAdd, kSub, kSub>(previous,
+                                            perspective,
+                                            king_square,
+                                            change.add_0,
+                                            change.add_1,
+                                            change.sub_0,
+                                            change.sub_1);
         break;
     }
   }
