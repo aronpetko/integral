4154825efa839e6d584e1bc6f00b853ef9b20689
aronpetko <aronpetko@gmail.com>
2025-04-29 15:26:30 -0500

    Bench: 2303903

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -988,7 +988,7 @@ Score Searcher::PVSearch(Thread &thread,
       // would cause a cutoff based on our current search window
       else if (tt_move_excluded_score >= beta &&
                std::abs(tt_move_excluded_score) < kTBWinInMaxPlyScore) {
-        return tt_move_excluded_score;
+        return (tt_move_excluded_score + beta) / 2;
       }
       // Negative Extensions: Search less since the TT move was not
       // singular, and it might cause a beta cutoff again.
