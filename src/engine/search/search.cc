c4c0c7f93333138a5a2c069643f28dfb65ba98eb
aronpetkovski <aronpetko@gmail.com>
2025-05-05 16:26:11 -0500

    Bench: 2276238

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -1091,7 +1091,17 @@ Score Searcher::PVSearch(Thread &thread,
 
         // Prevent re-searching if the new depth results in a search that is
         // equivalent to the reduced depth search
-        if (new_depth == reduced_depth) needs_full_search = false;
+        if (new_depth == reduced_depth) {
+          needs_full_search = false;
+        }
+
+        if (is_quiet) {
+          const int bonus = score <= alpha ? history::HistoryPenalty(new_depth)
+                          : score >= beta  ? history::HistoryBonus(depth)
+                                           : 0;
+          history.continuation_history->UpdateMoveScore(
+              board.GetStateHistory().Back(), move, bonus, stack);
+        }
       }
     } else {
       // If we didn't perform late move reduction, then we search this move at
@@ -1105,14 +1115,6 @@ Score Searcher::PVSearch(Thread &thread,
     if (needs_full_search) {
       score = -PVSearch<NodeType::kNonPV>(
           thread, new_depth, -alpha - 1, -alpha, stack + 1, !cut_node);
-
-      if (reduction != 0 && is_quiet) {
-        const int bonus = score <= alpha ? history::HistoryPenalty(new_depth)
-                        : score >= beta  ? history::HistoryBonus(depth)
-                                         : 0;
-        history.continuation_history->UpdateMoveScore(
-            board.GetStateHistory().Back(), move, bonus, stack);
-      }
     }
 
     // Perform a full window search on this move if it's known to be good
