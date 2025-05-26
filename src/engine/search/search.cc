763f4688370d7f628ae9119136bcecd3bb35dd50
aronpetko <aronpetko@gmail.com>
2025-05-26 12:46:00 -0500

    Bench: 2290153

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -1071,9 +1071,7 @@ Score Searcher::PVSearch(Thread &thread,
       }
 
       // Reduce less if the static evaluation has been corrected a lot
-      if (stack->eval_complexity > kLmrComplexityDiff) {
-        reduction -= kLmrComplexity;
-      }
+      reduction -= 32 * stack->eval_complexity / 8;
 
       // Reduce less if this move is a killer move
       if (move == stack->killer_moves[0] || move == stack->killer_moves[1]) {
