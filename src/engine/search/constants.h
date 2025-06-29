3f06d47b44d65cc398ccc741dbcc0d600e08ef32
aronpetko <aronpetko@gmail.com>
2025-06-29 14:06:03 -0500

    Bench: 2212385

--- src/engine/search/constants.h
+++ src/engine/search/constants.h
@@ -21,9 +21,9 @@ TUNABLE_STEP(kQsFailHighLerpFactor, 0.5777391111317516, 0.0, 1.0, false, 0.1);
 TUNABLE_STEP(kQsFutMargin, 156, 20, 300, false, 20);
 
 TUNABLE(kEvalHistUpdateMult, 60, 20, 100, false);
-TUNABLE(kEvalHistUpdateMin, 200, 5, 500, false);
-TUNABLE(kEvalHistUpdateMax, 200, 5, 500, false);
-TUNABLE_STEP(kEvalHistUpdateBias, 30, 0, 50, false, 5);
+TUNABLE(kEvalHistUpdateMin, 57, 5, 500, false);
+TUNABLE(kEvalHistUpdateMax, 123, 5, 500, false);
+TUNABLE_STEP(kEvalHistUpdateBias, 1, 0, 50, false, 5);
 
 TUNABLE(kRevFutDepth, 9, 4, 10, true);
 TUNABLE_STEP(kRevFutMargin, 51, 50, 150, false, 5);
