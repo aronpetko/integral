f3f391f4a4c751f850b1ccb00112f3fda40efa23
aronpetko <aronpetko@gmail.com>
2025-08-07 11:51:58 -0500

    Bench: 1822153

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -854,7 +854,7 @@ Score Searcher::PVSearch(Thread &thread,
 
   // Internal Iterative Reduction: Move ordering is expected to be worse with
   // no TT move, so we save time on searching this position now
-  if ((in_pv_node || cut_node) && depth >= kIirDepth &&
+  if ((in_pv_node || cut_node) && depth >= kIirDepth && stack->eval_complexity <= 15 * depth &&
       !stack->excluded_tt_move && (!tt_move || tt_entry->depth + 4 < depth)) {
     depth--;
   }
