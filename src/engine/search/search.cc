bbfba4878bb6c0d48150ea2e4c4ddbe46520c2b9
aronpetkovski <aronpetko@gmail.com>
2025-11-26 19:24:57 -0600

    Bench: 2461847

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -988,9 +988,8 @@ Score Searcher::PVSearch(Thread &thread,
       // move's search
       if (tt_move_excluded_score < new_beta) {
         // Extend more if the TT move is singular by a big margin
-        if (!in_pv_node &&
-            tt_move_excluded_score < new_beta - kSeDoubleMargin) {
-          extensions = 2 + (is_quiet && tt_move_excluded_score <
+        if (tt_move_excluded_score < new_beta - kSeDoubleMargin - 100 * in_pv_node) {
+          extensions = 2 + (!in_pv_node && is_quiet && tt_move_excluded_score <
                                             new_beta - kSeTripleMargin);
           depth += depth < kSeDepthExtensionDepth;
         } else {
