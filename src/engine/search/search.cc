77805d48b83adb92f9994f66c787e47231407c28
aronpetkovski <aronpetko@gmail.com>
2025-11-29 14:03:05 -0600

    Bench: 2155592

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -933,7 +933,7 @@ Score Searcher::PVSearch(Thread &thread,
           kFutMarginMult * lmr_fractional_depth / kLmrDepthScale +
           stack->history_score / kFutMarginHistDiv;
       if (lmr_depth <= kFutPruneDepth && !stack->in_check && is_quiet &&
-          stack->static_eval + futility_margin < alpha) {
+          stack->static_eval + futility_margin + stack->eval_complexity < alpha) {
         move_picker.SkipQuiets();
         continue;
       }
