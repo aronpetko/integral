3dd9c800dfd795dbf9d4488cae190571eeed6bc1
aronpetko <aronpetko@gmail.com>
2025-12-04 17:03:33 -0600

    Bench: 2036780

--- shared/nnue/definitions.h
+++ shared/nnue/definitions.h
@@ -10,7 +10,7 @@ namespace nnue {
 
 namespace arch {
 
-constexpr std::size_t kL1Size = 1536;
+constexpr std::size_t kL1Size = 256;
 constexpr std::size_t kL2Size = 16;
 constexpr std::size_t kL3Size = 32;
 constexpr std::size_t kInputBucketCount = 12;
@@ -24,15 +24,15 @@ constexpr std::int32_t kEvalScale = 200;
 }  // namespace arch
 
 // clang-format off
-struct RawNetwork {
-  MultiArray<I16, arch::kInputBucketCount, 2, PieceType::kNumPieceTypes, Squares::kSquareCount, arch::kL1Size> feature_weights;
-  MultiArray<I16, arch::kL1Size> feature_biases;
-  MultiArray<I8, arch::kOutputBucketCount, arch::kL2Size, arch::kL1Size> l1_weights;
-  MultiArray<float, arch::kOutputBucketCount, arch::kL2Size> l1_biases;
-  MultiArray<float, arch::kOutputBucketCount, arch::kL3Size, arch::kL2Size> l2_weights;
-  MultiArray<float, arch::kOutputBucketCount, arch::kL3Size> l2_biases;
-  MultiArray<float, arch::kOutputBucketCount, arch::kL3Size> l3_weights;
-  MultiArray<float, arch::kOutputBucketCount> l3_biases;
+struct alignas(simd::kAlignment) RawNetwork {
+  alignas(simd::kAlignment) MultiArray<I16, arch::kInputBucketCount, 2, PieceType::kNumPieceTypes, Squares::kSquareCount, arch::kL1Size> feature_weights;
+  alignas(simd::kAlignment) MultiArray<I16, arch::kL1Size> feature_biases;
+  alignas(simd::kAlignment) MultiArray<I8, arch::kOutputBucketCount, arch::kL2Size, arch::kL1Size> l1_weights;
+  alignas(simd::kAlignment) MultiArray<float, arch::kOutputBucketCount, arch::kL2Size> l1_biases;
+  alignas(simd::kAlignment) MultiArray<float, arch::kOutputBucketCount, arch::kL3Size, arch::kL2Size> l2_weights;
+  alignas(simd::kAlignment) MultiArray<float, arch::kOutputBucketCount, arch::kL3Size> l2_biases;
+  alignas(simd::kAlignment) MultiArray<float, arch::kOutputBucketCount, arch::kL3Size> l3_weights;
+  alignas(simd::kAlignment) MultiArray<float, arch::kOutputBucketCount> l3_biases;
 };
 
 struct alignas(simd::kAlignment) Network {
