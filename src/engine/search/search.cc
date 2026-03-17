e44b92189e5641626150457f33cfc73bba44280f
aronpetkovski <aronpetko@gmail.com>
2026-03-17 12:05:01 -0500

    Bench: 2166174

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -745,7 +745,7 @@ Score Searcher::PVSearch(Thread &thread,
     // Null Move Pruning: Forfeit a move to our opponent and cutoff if we
     // still have the advantage
     if (!(stack - 1)->move.IsNull() && stack->eval >= beta &&
-        stack->static_eval >= beta + kNmpBetaBase - kNmpBetaMult * depth &&
+        stack->static_eval >= beta + kNmpBetaBase - kNmpBetaMult * depth + 100 * !cut_node &&
         !stack->excluded_tt_move && stack->ply >= thread.nmp_min_ply) {
       // Avoid null move pruning a position with high zugzwang potential
       const BitBoard non_pawn_king_pieces =
