03fdd3e1e33d5bb7dc5c199dd68e907cf9529f99
aronpetkovski <aronpetko@gmail.com>
2026-03-19 16:37:44 -0500

    Bench: 2027832

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -1063,6 +1063,10 @@ Score Searcher::PVSearch(Thread &thread,
       // Reduce more if this node is expected to fail high
       if (cut_node) {
         reduction += kLmrCutNode;
+
+        if (move_picker.GetStage() == MovePicker::Stage::kBadNoisys) {
+          reduction += 512;
+        }
       }
 
       // Reduce less if this move gives check
