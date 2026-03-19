b1df52b9f9d1dd71db6cca2e61afe322aefe18f5
aronpetkovski <aronpetko@gmail.com>
2026-03-19 12:57:18 -0500

    Bench: 1906543

--- src/engine/search/search.cc
+++ src/engine/search/search.cc
@@ -1084,7 +1084,7 @@ Score Searcher::PVSearch(Thread &thread,
       }
 
       // Reduce less if the static evaluation has been corrected a lot
-      reduction -= std::abs(stack->correction_value / 16384);
+      reduction -= std::abs(stack->correction_value / 65536);
 
       // Reduce less if this move is a killer move
       if (move == stack->killer_moves[0] || move == stack->killer_moves[1]) {
