55f8113512bddc2673cd79b51eef59299532dc3b
aronpetko <aronpetko@gmail.com>
2026-08-31 14:01:02 -0500

    Bench: 2250346

--- shared/nnue/definitions.h
+++ shared/nnue/definitions.h
@@ -20,7 +20,7 @@ constexpr std::size_t kOutputBucketCount = 8;
 constexpr std::int32_t kFtQuantization = 255;
 constexpr std::int32_t kL1Quantization = 128;
 
-constexpr std::int32_t kEvalScale = 200;
+constexpr std::int32_t kEvalScale = 300;
 
 }  // namespace arch
 
