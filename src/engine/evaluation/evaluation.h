cc1321bfb553c2f7c1b70958b7e2f9464b49ab96
aronpetko <aronpetko@gmail.com>
2026-09-11 06:33:33 -0500

    Normalize scores and prepare for release
    Bench: 2276423

--- src/engine/evaluation/evaluation.h
+++ src/engine/evaluation/evaluation.h
@@ -28,7 +28,7 @@ inline std::array kSeePieceScores = {
 
 // Constants derived from WDL_model
 constexpr std::array kAs = {
-    86.66506958, -259.82299819, 205.33735813, 127.53923650};
+    -68.80447839, 169.28348984, -154.00729306, 235.96617727};
 
 [[nodiscard]] static Score NormalizeScore(Score score, int material_count) {
   if (score == 0 || std::abs(score) >= kTBWinInMaxPlyScore) return score;
--- src/engine/uci/uci.h
+++ src/engine/uci/uci.h
@@ -14,7 +14,7 @@ namespace uci {
 
 namespace constants {
 
-constexpr std::string_view kEngineName = "Integral v7.0.0-dev";
+constexpr std::string_view kEngineName = "Integral v8";
 constexpr std::string_view kEngineAuthor = "Aron Petkovski";
 
 }  // namespace constants
