c77ced6c9a52f799680d08a3f9838a34f7c9e4bd
Aron Petkovski <aronpetko@gmail.com>
2026-09-19 20:03:36 -0500

    Switch to the `Bulbasaur` network (#253)
    Bench: 2112443

---
+++ src/engine/evaluation/nnue/pawn_pair/pawn_pair_features.cc
@@ -0,0 +1,5 @@
+//
+// Created by Aron on 9/20/2026.
+//
+
+#include "pawn_pair_features.h"
---
+++ src/engine/evaluation/nnue/pawn_pair/pawn_pair_features.h
@@ -0,0 +1,16 @@
+//
+// Created by Aron on 9/20/2026.
+//
+
+#ifndef SPRT_SH_PAWN_PAIR_FEATURES_H
+#define SPRT_SH_PAWN_PAIR_FEATURES_H
+
+
+
+class pawn_pair_features {
+
+};
+
+
+
+#endif //SPRT_SH_PAWN_PAIR_FEATURES_H
